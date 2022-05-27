8ce3d1e1434f52672f4db9602a3b2438931aa200
Tomasz Jarko <tomasz.jarko@gmail.com>
2022-05-27 18:50:47 +0200

    Updating utils

--- main/universities/utils.py
+++ main/universities/utils.py
@@ -1,23 +1,34 @@
 from universities import models
 import requests
 
+MAIN_URL = 'http://universities.hipolabs.com/search?'
 
 def get_universities_json():
-    url = 'http://universities.hipolabs.com/search?country=Poland'
-    response = requests.get(url)
+    response = requests.get(MAIN_URL)
     data = response.json()
-
-    keys = ["web_pages", "name", "alpha_two_code", "state-province",  "domains", "country"]
-    poland = models.Country.objects.get(country_name="Poland")
 
     for school in data:
         university = models.University.objects.create(
             code = str(school["alpha_two_code"]),
-            country = models.Country.objects.get(country_name="Poland"),
+            country = models.Country.objects.get(country_name=school["country"]),
             state_province = school["state-province"],
             domains = str(school["domains"]),
             name = str(school["name"]),
             web_pages = str(school["web_pages"])
         )
         university.save()
+    return "SUCCESS"
+
+def get_countries():
+    response = requests.get(MAIN_URL)
+    data = response.json()
+
+    for school in data:
+        if models.Country.objects.filter(country_name=school["country"]).exists() == True:
+            continue
+        else:
+            country = models.Country.objects.create(
+                country_name = str(school["country"])
+            )
+            country.save()
     return "SUCCESS"
