a0632764b69cb9f549cd181140ef0264de4ece77
Tomasz Jarko <tomasz.jarko@gmail.com>
2022-06-15 23:51:00 +0200

    Adding some files

--- main/universities/urls.py
+++ main/universities/urls.py
@@ -1,17 +1,9 @@
-from django.urls import include, re_path
+from django.urls import re_path
 from rest_framework import routers
 from . import views
 
 
-# router = routers.DefaultRouter()
-# router.register(r"universities", views.UniversityViewSet, basename="universities")
-# router.register(r"country", views.CountryViewSet, basename="country")
-
 urlpatterns = [
-    #re_path(r"^", include(router.urls)),
     re_path("search/", views.SearchResultsView.as_view(), name="search_results"),
     re_path("home/", views.HomePageView.as_view(), name="home"),
-    re_path("universities/", views.UniversityView.as_view(), name="universities"),
-    # re_path("uni/", views.UniversityViewSet.as_view(), name="universities"),
-    # re_path("country/", views.CountryViewSet.as_view(), name="country")
 ]
--- main/universities/views.py
+++ main/universities/views.py
@@ -3,7 +3,17 @@ from rest_framework.viewsets import ModelViewSet
 from django.views.generic import TemplateView, ListView
 from universities import models
 from django.db.models import Q
-from django.views import generic
+from django.shortcuts import render
+
+
+def get_data(request):
+    
+    my_data = models.University.objects.all()
+    
+    context={
+      'my_data':my_data,
+    } 
+    return render(request, 'universities_list.html', context)
 
 
 class UniversityViewSet(ModelViewSet):
@@ -25,9 +35,10 @@ class CountryViewSet(ModelViewSet):
 class HomePageView(TemplateView):
     template_name = 'home_page.html'
 
+
 class UniversityView(TemplateView):
-    model = models.University
     template_name = 'universities_list.html'
+
 
 class SearchResultsView(ListView):
     model = models.University
